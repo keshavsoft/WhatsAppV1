7779fb1483670b18878cad774597a687ed9f3572
keshavsoft <nknnkumar@yahoo.com>
2025-05-21 17:02:19 +0530

    chats link added and public

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 import { StartFunc as StartFuncPortListen } from "./PortListen.js";
 
 import { StartFunc as StartFuncKWSServer } from "./Projects/KWSServer/EntryFile.js";
+import { router as routerFromDataFolder } from "./DataFolder/routes.js";
+
 import { StartFunc as StartFuncFromEntryFile } from "./WA/entryFile.js";
 
 import express from 'express';
@@ -25,6 +27,7 @@ var port = normalizePort(process.env.PORT || '7019');
 app.use(cookieParser());
 
 app.use('/', express.static(path.join(path.resolve(), 'public')));
+app.use("/DataFolder", routerFromDataFolder);
 
 app.get('/StartWA', async (req, res) => {
     await StartFuncFromEntryFile({ inReponse: res });
